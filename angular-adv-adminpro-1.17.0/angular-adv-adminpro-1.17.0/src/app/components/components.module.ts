ca6d096663037639dcf56638833ebe083e35aac0
Raul Armas <you@example.com>
2022-05-30 22:21:39 -0500

    update

--- angular-adv-adminpro-1.17.0/angular-adv-adminpro-1.17.0/src/app/components/components.module.ts
+++ angular-adv-adminpro-1.17.0/angular-adv-adminpro-1.17.0/src/app/components/components.module.ts
@@ -2,12 +2,11 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms'
 
-// import { ChartsModule } from 'ng2-charts';
+import { NgChartsModule } from 'ng2-charts';
 
 import { IncrementadorComponent } from './incrementador/incrementador.component';
 import { DonaComponent } from './dona/dona.component';
 import { ModalImagenComponent } from './modal-imagen/modal-imagen.component';
-
 
 
 @NgModule({
--- angular-adv-adminpro-1.17.0/angular-adv-adminpro-1.17.0/src/app/components/dona/dona.component.ts
+++ angular-adv-adminpro-1.17.0/angular-adv-adminpro-1.17.0/src/app/components/dona/dona.component.ts
@@ -1,52 +1,65 @@
-import { Component, Input } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import {Chart, ChartConfiguration, ChartItem, registerables} from 'node_modules/chart.js';
 
 @Component({
   selector: 'app-dona',
-  templateUrl: './dona.component.html',
-  styles: [
-  ]
+  templateUrl: './dona.component.html'
 })
 
 
-export class DonaComponent{
+export class DonaComponent implements OnInit{
   
-  // constructor() { }
+  constructor() { }
 
-  // ngOnInit(): void {
-  //   this.createChart()
-  // }
+   ngOnInit(): void {
+     this.createChart()
+   }
 
-  // createChart(): void {
-  //   Chart.register(...registerables)
+   createChart(): void {
+     Chart.register(...registerables)
 
-  //   const data = {
-  //     labels: ['January','February','March','April','May'],
-  //     datasets: [{
-  //       label: 'My First dataset',
-  //       backgroundColor: 'rgb(255, 99, 132)',
-  //       borderColor: 'rgb(255, 99, 132)',
-  //       data: [10, 5, 2, 20, 30, 45],
-  //     }]
-  //   };
+    const data = {
+       labels: ['Enero','Febrero','Marzo','Abril','Mayo'],
+     datasets: [{
+        label: 'My First dataset',
+         backgroundColor: 'rgb(255, 99, 132)',
+         borderColor: 'rgb(255, 99, 132)',
+        data: [10, 5, 2, 20, 30, 45],
+       }]
+     };
 
-  //   const options = {
-  //     scales: {
-  //       y: {
-  //         beginAtZero: true,
-  //         display: false
-  //       }
-  //     }
-  //   }
+      const options = {
+       scales: {
+          y: {
+           beginAtZero: true,
+            display: false
+         }
+       }
+     }
 
-  //   const config: ChartConfiguration = {
-  //     type: 'line',
-  //     data: data,
-  //     options: options
-  //   }
+     const config: ChartConfiguration = {
+       type: 'bar',
+       data: data,
+       options: options
+     }
 
-  //   const chartItem: ChartItem = document.getElementById('my-chart') as ChartItem
 
-  //   new Chart(chartItem, config)
-  // }
+     var els = document.getElementsByClassName("my-chart");
+
+     Array.prototype.forEach.call(els, function(el) {
+      // Do stuff here
+      const chartItem: ChartItem = el as ChartItem
+      new Chart(chartItem, config)
+  });
+
+
+    //  [...document.getElementsByClassName("mychartclass")]
+    //  document.getElementsByClassName('my-chart').forEach(element => {
+    //   const chartItem: ChartItem = element as ChartItem
+    //   new Chart(chartItem, config)
+    //  });
+     //getElementById('my-chart') as ChartItem
+
+  
+   }
 }
