c1881076e5077e2054e5717407d3c3e0212ce2aa
Raul Armas <you@example.com>
2022-06-07 13:55:41 -0500

    modified the library chart js to a deprecated version

--- angular-adv-adminpro-1.17.0/angular-adv-adminpro-1.17.0/src/app/components/dona/dona.component.ts
+++ angular-adv-adminpro-1.17.0/angular-adv-adminpro-1.17.0/src/app/components/dona/dona.component.ts
@@ -1,65 +1,24 @@
 import { Component, OnInit } from '@angular/core';
-import {Chart, ChartConfiguration, ChartItem, registerables} from 'node_modules/chart.js';
-
+import { ChartOptions, ChartType, ChartDataSets } from 'chart.js';
+import { Label } from 'ng2-charts';
 @Component({
   selector: 'app-dona',
   templateUrl: './dona.component.html'
 })
 
 
-export class DonaComponent implements OnInit{
+export class DonaComponent{
   
-  constructor() { }
-
-   ngOnInit(): void {
-     this.createChart()
-   }
-
-   createChart(): void {
-     Chart.register(...registerables)
-
-    const data = {
-       labels: ['Enero','Febrero','Marzo','Abril','Mayo'],
-     datasets: [{
-        label: 'My First dataset',
-         backgroundColor: 'rgb(255, 99, 132)',
-         borderColor: 'rgb(255, 99, 132)',
-        data: [10, 5, 2, 20, 30, 45],
-       }]
-     };
-
-      const options = {
-       scales: {
-          y: {
-           beginAtZero: true,
-            display: false
-         }
-       }
-     }
-
-     const config: ChartConfiguration = {
-       type: 'bar',
-       data: data,
-       options: options
-     }
-
-
-     var els = document.getElementsByClassName("my-chart");
-
-     Array.prototype.forEach.call(els, function(el) {
-      // Do stuff here
-      const chartItem: ChartItem = el as ChartItem
-      new Chart(chartItem, config)
-  });
-
-
-    //  [...document.getElementsByClassName("mychartclass")]
-    //  document.getElementsByClassName('my-chart').forEach(element => {
-    //   const chartItem: ChartItem = element as ChartItem
-    //   new Chart(chartItem, config)
-    //  });
-     //getElementById('my-chart') as ChartItem
+  barChartOptions: ChartOptions = {
+    responsive: true,
+  };
+  barChartLabels: Label[] = ['Apple', 'Banana', 'Kiwifruit', 'Blueberry', 'Orange', 'Grapes'];
+  barChartType: ChartType = 'bar';
+  barChartLegend = true;
+  barChartPlugins = [];
+  barChartData: ChartDataSets[] = [
+    { data: [45, 37, 60, 70, 46, 33], label: 'Best Fruits' }
+  ];
 
   
    }
-}
